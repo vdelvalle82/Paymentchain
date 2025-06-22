5ef4703fc1662b930b119d97bf0a442a62db3ac7
Virginia <vdelvalle82@hotmail.com>
2025-06-22 21:27:13 +0200

    Primer commit del proyecto Paymentchain microservicios

---
+++ paymentchainparent/bussinessdomain/src/main/java/com/paymentchain/bussinessdomain/App.java
@@ -0,0 +1,10 @@
+package com.paymentchain.bussinessdomain;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
